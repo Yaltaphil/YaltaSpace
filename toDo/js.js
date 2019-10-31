3ee70842e5c09f24cc28f3fef0e151e7a1d11d2f
Yaltaphil <yaltaphil@yandex.ru>
2019-10-31 14:33:47 +0300

    переделан

--- toDo/js.js
+++ toDo/js.js
@@ -1,66 +1,56 @@
 
+const
+    list = document.querySelector("ul"),
+    inputField = document.getElementById("newItem");
 
-var list = document.querySelector("ul");
+var
+    toDo;
 
-
-var toDo;
 
 //Записыват список во временное хранилище браузера
 function Store() {
     toDo = list.innerHTML;
-    localStorage.setItem('toDo', toDo);
+    localStorage.setItem('toDoList', toDo);
 }
-
-
-list.addEventListener('click', processItem);
-
 
 //Обработка клика на элементе (смена статуса или удаление)
 function processItem(listElement) {
-
     if (listElement.target.tagName === 'LI') {
         listElement.target.classList.toggle('checked');
         Store();
     }
     else if (listElement.target.tagName === 'SPAN') {
-        var div = listElement.target.parentNode;
-        div.remove();
+        listElement.target.parentNode.remove();
         Store();
     }
 }
 
-
-
 function addItem() {
-
-    var li = document.createElement('li'),
-        inputValue = document.getElementById("newItem").value,
-        t = document.createTextNode(inputValue);
-
-    li.appendChild(t);
-
-    if (inputValue == "") {
-        alert("Нужно ввести дело!");
+    if (inputField.value === "") {
+        inputField.style.transform = "rotateY(360deg)";
     } else {
+        let li = document.createElement('li'),
+            textNode = document.createTextNode(inputField.value);
+        li.appendChild(textNode);
         document.getElementById("list").appendChild(li);
+        let span = document.createElement('SPAN'),
+            txt = document.createTextNode('X');
+        span.className = 'close';
+        span.appendChild(txt);
+        li.appendChild(span);
+        inputField.value = "";
+        Store();
     }
-
-    document.getElementById('newItem').value = "";
-
-    var span = document.createElement('SPAN'),
-        txt = document.createTextNode('X');
-
-    span.className = 'close';
-    span.appendChild(txt);
-    li.appendChild(span);
-    Store();
 }
-
 
 //Если есть сохраненные задачи - загрузить
-if (localStorage.getItem('toDo')) {
-    list.innerHTML = localStorage.getItem('toDo');
+if (localStorage.getItem('toDoList')) {
+    list.innerHTML = localStorage.getItem('toDoList');
 }
+
+
+list.addEventListener('click', processItem);
+
 
 //ввод в помощью Enter
 document.addEventListener('keypress',
