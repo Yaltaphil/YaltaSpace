5a246c43055fc3d4019057055bae39d1ed5b4792
Yaltaphil <yaltaphil@yandex.ru>
2020-01-28 14:17:05 +0300

    feat: tabs module connected

---
+++ yoga/js/script.js
@@ -0,0 +1,11 @@
+
+import {initTabs} from './tabs.js';
+
+
+window.addEventListener('DOMContentLoaded', function () {
+
+    initTabs('info-header',
+        'info-header-tab',
+        'info-tabcontent');
+
+});
