1c0d2ab7b25b82eb87ddbec71c507444d0128086
Yaltaphil <yaltaphil@yandex.ru>
2020-01-29 15:26:03 +0300

    yoga addded

--- yoga/js/tabs.js
+++ yoga/js/tabs.js
@@ -1,4 +1,4 @@
-function initTabs(panel, buttons, content) {
+export function initTabs(panel, buttons, content) {
 
     'use strict';
 
@@ -9,7 +9,7 @@ function initTabs(panel, buttons, content) {
         tabContent = document.querySelectorAll('.' + content);
 
 
-    function hideAll() {
+    function hideAllTabContent() {
         tabContent.forEach((tab) => {
             tab.classList.remove('show');
             tab.classList.add('hide');
@@ -25,27 +25,19 @@ function initTabs(panel, buttons, content) {
     }
 
 
-    hideAll();
+    hideAllTabContent();
 
     showTabContent(0);
 
     tabPanel.addEventListener('click', function (event) {
         let target = event.target;
-        // console.log( Array.prototype.indexOf.call(this.children, target));
-        // console.log( [...target.parentNode.children].indexOf(target));
-        // console.log( [...this.children].indexOf(target));
+        // console.log( Array.from(this.children).indexOf(target) );
+        // console.log( Array.prototype.indexOf.call(this.children, target) );
+        // console.log( [...target.parentNode.children].indexOf(target) );
+        // console.log( [...this.children].indexOf(target) );
         if (target.classList.contains(buttons)) {
-            hideAll();
-            showTabContent([...this.children].indexOf(target));
+            hideAllTabContent();
+            showTabContent( Array.from(this.children).indexOf(target) );
         }
     });
 }
-
-
-export {initTabs};
-
-
-
-
-
-
